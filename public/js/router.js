93bf6c4571b9411f421c7ee543cd4354a0f710fd
vanefRiwi <125419162+vanefRiwi@users.noreply.github.com>
2026-05-24 12:55:08 -0500

    Update router.js

--- public/js/router.js
+++ public/js/router.js
@@ -33,7 +33,7 @@ export function navigateTo(routeName) {
     console.warn(`Router: la ruta "${routeName}" no existe.`);
     return;
   }
-  //history.pushState({ route: routeName }, '', `/${routeName}`);
+ 
 
   // Vista actualmente visible
   const current = document.querySelector('.view.active');
